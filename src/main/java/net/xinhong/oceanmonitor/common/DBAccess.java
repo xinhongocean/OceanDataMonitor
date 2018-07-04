5bbbf26d0104856ffb3069bb6c2845fa314bc467
momogoutou <910139735@qq.com>
2018-07-04 17:01:28 +0800

    修改某些不规范代码

--- src/main/java/net/xinhong/oceanmonitor/common/DBAccess.java
+++ src/main/java/net/xinhong/oceanmonitor/common/DBAccess.java
@@ -3,29 +3,32 @@ package net.xinhong.oceanmonitor.common;
 /**
  * Created by zxf on 2018/3/26.
  */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 public class DBAccess {
     //获取数据库连接
-    public Connection getConnection(){
+    public Connection getConnection() {
         Connection con = null;
         try {
-//加载数据库驱动程序
+            //加载数据库驱动程序
             Class.forName("oracle.jdbc.driver.OracleDriver");
-//获取数据库链接对象
-            con=DriverManager.getConnection("jdbc:oracle:thin:@192.6.1.112:1521:xhdb","qhztj","qhztj123");
+            //获取数据库链接对象
+            con = DriverManager.getConnection("jdbc:oracle:thin:@192.6.1.112:1521:xhdb", "qhztj", "qhztj123");
         } catch (Exception e) {
             e.printStackTrace();
         }
-//返回链接对象
+        //返回链接对象
         return con;
     }
+
     //测试
     public static void main(String[] args) {
         DBAccess dbac = new DBAccess();
-        if(dbac.getConnection()!=null){
+        if (dbac.getConnection() != null) {
             System.out.println("Get connnection success!");
         }
     }
--- src/main/java/net/xinhong/oceanmonitor/controller/RedisController.java
+++ src/main/java/net/xinhong/oceanmonitor/controller/RedisController.java
@@ -7,7 +7,6 @@ import net.xinhong.oceanmonitor.common.ResStatus;
 import net.xinhong.oceanmonitor.service.impl.RedisKeyServiceImpl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
